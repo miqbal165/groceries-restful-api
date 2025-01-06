c2b2d52840b6fa56be7ca621924f57d77149d8ec
Iqbal16 <miqbaal1605@gmail.com>
2025-01-06 13:23:57 +0700

    init: First commit for ignore file.

---
+++ src/main/kotlin/com/mohiqbaldev/groceries_restful_api/GroceriesRestfulApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mohiqbaldev.groceries_restful_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroceriesRestfulApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GroceriesRestfulApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mohiqbaldev/groceries_restful_api/GroceriesRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mohiqbaldev.groceries_restful_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GroceriesRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
